c8f8faab901b0841dcc170940d962afdeb9bf263
Phil McCarthy <Philip.McCarthy@renishaw.com>
2013-10-15 13:36:20 +0100

    Added unit tests to test specific types of string difference for better debugging and analysis

--- test/LambdaTests.cpp
+++ test/LambdaTests.cpp
@@ -16,72 +16,109 @@
 
 using namespace lambda;
 
-void AnalyseLambdaBuffer(const BYTEVECTOR& lambdaBuffer);
-
-TEST_CASE( "Lambda basic encode and decode", "[CMLZ03Encoder]" )
+///////////////////////////////////////////////////////////////////////////////
+/// Utility function to convert std::string to CDataBuffer. Assumes STL string
+/// implementation uses contiguous memory (very safe bet!)
+///
+/// \param [in] const std::string& stringRef - the string to convert to CDataBuffer
+/// \return CDataBuffer - the converted string
+///////////////////////////////////////////////////////////////////////////////
+CDataBuffer StringToDataBuffer(const std::string& stringRef)
 {
-    CDataBuffer testStringBuf((const PBYTE)&TEST_STRING[0], TEST_STRING.size());
-    CDataBuffer updatedStringBuf((const PBYTE)&UPDATED_STRING[0], UPDATED_STRING.size());
+	return CDataBuffer((const lambda::PBYTE)&stringRef[0], stringRef.size());
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// Utility function to convert CDataBuffer to std::string. Assumes STL string
+/// implementation uses contiguous memory (very safe bet!)
+///
+/// \param [in] const CDataBuffer& dataBuffer - the buffer to convert to std::string
+/// \return std::string - the converted buffer
+///////////////////////////////////////////////////////////////////////////////
+std::string DataBufferToString(const CDataBuffer& dataBuffer)
+{
+	return std::string((char*)dataBuffer.Buffer(), dataBuffer.Size());
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// Utility function containing the boilerplate for taking a reference and test
+/// string, creating lambda data from them, and then applying the lambda to the
+/// reference string to get the result. Used repeatedly to test that strings can
+/// be modified in different ways, and lambda encode/decode still works.
+///
+/// \param [in] const std::string& refString - the reference (original) string
+/// \param [in] const std::string& testString - reString modified in some way
+/// \return std::string - the result of the lambda decode.
+///////////////////////////////////////////////////////////////////////////////
+std::string TestEncodeDecode(const std::string& refString, const std::string& testString)
+{
+	CDataBuffer refStringBuf = StringToDataBuffer(refString);
+    CDataBuffer testStringBuf = StringToDataBuffer(testString);
     
+	// do the difference encode
     CMLZ03Codec codec;
-    // do the difference encode
-    CDataBuffer lambdaBuf = codec.EncodeBuffer(testStringBuf, updatedStringBuf);
-    
-    // look at the diff buffer
-    /*
-    AnalyseLambdaBuffer(lambdaBuf);
-    std::cout << std::endl;
-    std::cout <<  "Modified buf=" << updatedBuf.size() << " bytes, Lambda buf=" << lambdaBuf.size() << " bytes, ";
-    double dbCR = 100 - ((double)lambdaBuf.size() / (double)updatedBuf.size()) * 100;
-    std::cout << "Compression ratio = " << dbCR << "%." << std::endl;
-    */
+    CDataBuffer lambdaBuf = codec.EncodeBuffer(refStringBuf, testStringBuf);
     
     // apply the lambda to the original buffer to get back the modified buffer
-    CDataBuffer modifiedBuf = codec.DecodeBuffer(testStringBuf, lambdaBuf);
-    std::string strAppliedString((char*)modifiedBuf.Buffer(), modifiedBuf.Size());
-    
-    // assert that the input for the operation is the same as the output
-    REQUIRE( UPDATED_STRING == strAppliedString );
+    CDataBuffer modifiedBuf = codec.DecodeBuffer(refStringBuf, lambdaBuf);
+	return DataBufferToString(modifiedBuf);
 }
 
-TEST_CASE( "Lambda compression size", "[CMLZ03Encoder]" )
+TEST_CASE( "Test encoding string with inserted text at the beginning", "[CMLZ03Encoder]" )
 {
-    CDataBuffer testStringBuf((const PBYTE)&TEST_STRING[0], TEST_STRING.size());
-    CDataBuffer updatedStringBuf((const PBYTE)&UPDATED_STRING[0], UPDATED_STRING.size());
+	std::cout << std::endl << "Test encoding string with inserted text at the beginning" << std::endl;
+    std::string decodedString = TestEncodeDecode(REFERENCE_STRING, INSERTION_AT_BEGINNING);
+    REQUIRE( decodedString == INSERTION_AT_BEGINNING );
+}
+
+TEST_CASE( "Test encoding string with inserted text at the end", "[CMLZ03Encoder]" )
+{
+	std::cout << std::endl << "Test encoding string with inserted text at the end" << std::endl;
+	std::string decodedString = TestEncodeDecode(REFERENCE_STRING, INSERTION_AT_END);
+    REQUIRE( decodedString == INSERTION_AT_END );
+}
+
+TEST_CASE( "Test encoding string with end section moved to beginning", "[CMLZ03Encoder]" )
+{
+	std::cout << std::endl << "Test encoding string with end section moved to beginning" << std::endl;
+	std::string decodedString = TestEncodeDecode(REFERENCE_STRING, SECTION_MOVE_TO_BEGINNING);
+    REQUIRE( decodedString == SECTION_MOVE_TO_BEGINNING );
+}
+
+TEST_CASE( "Test encoding string with beginning part deleted", "[CMLZ03Encoder]" )
+{
+	std::cout << std::endl << "Test encoding string with beginning part deleted" << std::endl;
+	std::string decodedString = TestEncodeDecode(REFERENCE_STRING, BEGINNING_DELETED);
+    REQUIRE( decodedString == BEGINNING_DELETED );
+}
+
+TEST_CASE( "Test encoding string with end part deleted", "[CMLZ03Encoder]" )
+{
+	std::cout << std::endl << "Test encoding string with end part deleted" << std::endl;
+	std::string decodedString = TestEncodeDecode(REFERENCE_STRING, END_DELETED);
+    REQUIRE( decodedString == END_DELETED );
+}
+
+TEST_CASE( "Test encoding string with different insertions, rewrites and deletions", "[CMLZ03Encoder]" )
+{
+	std::cout << std::endl << "Test encoding string with different insertions, rewrites and deletions" << std::endl;
+	std::string decodedString = TestEncodeDecode(REFERENCE_STRING, DELETIONS_AND_INSERTIONS);
+    REQUIRE( decodedString == DELETIONS_AND_INSERTIONS );
+}
+
+TEST_CASE( "Lambda size is less than modified string", "[CMLZ03Encoder]" )
+{
+	std::cout << std::endl << "Lambda size is less than modified string" << std::endl;
+
+    CDataBuffer refStringBuf = StringToDataBuffer(REFERENCE_STRING);
+    CDataBuffer testStringBuf = StringToDataBuffer(DELETIONS_AND_INSERTIONS);
     
+	// do the difference encode
     CMLZ03Codec codec;
-    // do the difference encode
-    CDataBuffer lambdaBuf = codec.EncodeBuffer(testStringBuf, updatedStringBuf);
+    CDataBuffer lambdaBuf = codec.EncodeBuffer(refStringBuf, testStringBuf);
     
     // assert that lambda compressed form has actually saved space compared to the data being encoded
-    REQUIRE( lambdaBuf.Size() < updatedStringBuf.Size() );
+    REQUIRE( lambdaBuf.Size() < DELETIONS_AND_INSERTIONS.size() );
 }
 
-/*
-// print out the deserialised contents of the lambda buffer
-void AnalyseLambdaBuffer(const BYTEVECTOR& lambdaBuffer)
-{
-	ULONG nLambdaBufferPos = 0;
-	while (nLambdaBufferPos < lambdaBuffer.size())
-	{
-		do // do-while-false flat handling construct
-		{
-			std::unique_ptr<lambda::CCopyOperation> pCopyOp = lambda::CCopyOperation::TryDeserialise(lambdaBuffer, &nLambdaBufferPos);
-			if (pCopyOp != nullptr)
-			{
-				pCopyOp->Print();
-				break;
-			}
-			
-			std::unique_ptr<lambda::CInsertOperation> pInsertOp = lambda::CInsertOperation::TryDeserialise(lambdaBuffer, &nLambdaBufferPos);
-			if (pInsertOp != nullptr)
-			{
-				pInsertOp->Print();
-				break;
-			}
-            
-		} while (false);
-        
-	}
-}
-*/
+//TODO: Add tests for CPatchFile here
